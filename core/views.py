cdd7292d4ff95fb8975667a1442b032cfdd1e584
vikornienko <vikornienko76@gmail.com>
2023-04-20 05:27:50 +0500

    Fifth commit. Added templates for shop, product pages. Added appproduct view.

--- core/views.py
+++ core/views.py
@@ -6,3 +6,8 @@ from appproduct.models import Product
 def frontpage(request):
     products = Product.objects.all()[:8]
     return render(request, "core/frontpage.html", {'products': products})
+
+
+def shop(request):
+    products = Product.objects.all()
+    return render(request, "core/shop.html", {"products": products})
--- djangoHtmxTailwind/urls.py
+++ djangoHtmxTailwind/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from core.views import frontpage
+from core.views import frontpage, shop
+from appproduct.views import product
 
 urlpatterns = [
     path('', frontpage, name='frontpage'),
+    path('shop/', shop, name='shop'),
+    path('product/', product, name='product'),
     path('admin/', admin.site.urls),
 ]
