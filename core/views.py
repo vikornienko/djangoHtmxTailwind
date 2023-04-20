e3f201d035c0e16c675532616999b4b9c48b84dc
vikornienko <vikornienko76@gmail.com>
2023-04-20 06:03:16 +0500

    The sixth commit Search is implemented.

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.db.models import Q
 
-from appproduct.models import Product
+from appproduct.models import Product, Category
 
 # Create your views here.
 def frontpage(request):
@@ -9,5 +10,19 @@ def frontpage(request):
 
 
 def shop(request):
+    categories = Category.objects.all()
     products = Product.objects.all()
-    return render(request, "core/shop.html", {"products": products})
+    active_category = request.GET.get('category', '')
+
+    if active_category:
+        products = products.filter(category__slug=active_category)
+
+    query = request.GET.get('query', '')
+    if query:
+        products = products.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    page_context = {
+        'categories': categories,
+        'products': products,
+        'active_category': active_category
+    }
+    return render(request, "core/shop.html", page_context)
